815ff42e0cd4a45e1c41a700f1dc564061a6726a
aniar <aniar.kaldybaiuly@nu.edu.kz>
2023-07-04 19:17:20 +0600

    1.0

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,16 @@
 package com.epam.demo;
 
 import java.util.List;
+import org.example.*;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
+        for (String arg : args) {
+            if ( !isPositiveNumber(arg)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,14 @@
 package com.epam.utils;
+import org.apache.commons.*;
+
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
+        if( createNumber(str).doubleValue() > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
